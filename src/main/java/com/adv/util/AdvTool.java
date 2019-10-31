2ad4db738dbc525595633f91cc8a00a5a01ada08
dtvikey888 <dtvikey@163.com>
2019-10-31 15:58:14 +0800

    test

--- src/main/java/com/adv/util/AdvTool.java
+++ src/main/java/com/adv/util/AdvTool.java
@@ -1,6 +1,8 @@
 package com.adv.util;
 
 import java.sql.ResultSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @ClassName $ {NAME}
@@ -11,16 +13,50 @@ import java.sql.ResultSet;
  **/
 public class AdvTool {
     //完善资料
-    public static void wszl(String memo,String xm,String tel){
+    public static void wszl(String memo,String xm,String tel,String msgtel){
         try {
 
             MysqlDB db = new MysqlDB();
             String sql = "insert into adv(xm,tel,memo,create_time) values('"+xm+"','"+tel+"','"+memo+"','"+TimeString.nowTime()+"')";
             db.executeInsert(sql);
 
+            //发短信功能
+            if(AdvTool.isMobileNO(msgtel)){
+
+                SmsDemo app = new SmsDemo();
+
+                String content="";
+                if (memo.length()>30) {
+                    content=memo.substring(0,30);
+                }else {
+                    content=memo;
+                }
+
+
+                String time=TimeString.nowTime();
+
+
+                app.sendMsg(""+msgtel+"","{\"name\":\""+xm+"\", \"tel\":\""+tel+"\", \"content\":\""+content+"\", \"time\":\""+time+"\"}");
+
+
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    //java-正则表达式判断是否手机号
+    public static boolean isMobileNO(String mobiles){
+
+        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
+
+        Matcher m = p.matcher(mobiles);
+
+        //System.out.println(m.matches());
+
+        return m.matches();
+
     }
 
 }
--- src/main/webapp/tj_chk.jsp
+++ src/main/webapp/tj_chk.jsp
@@ -1,11 +1,18 @@
 <%@ page import="com.adv.util.AdvTool" %>
 <%@ page contentType="text/html;charset=utf-8" language="java" %>
 <%
+    request.setCharacterEncoding("utf-8");
     String memo =  request.getParameter("memo");
     String xm   =  request.getParameter("xm");
     String tel  =  request.getParameter("tel");
 
-    AdvTool.wszl(memo,xm,tel);
+//    out.println(xm);
+//    out.println(memo);
+//    out.close();
+    //String msgtel="13706776231";
+    String msgtel="13588963940";
+
+    AdvTool.wszl(memo,xm,tel,msgtel);
 
     out.print("<script type=\"text/javascript\">document.location.href='index.jsp';</script>");
 //out.print("<script type=\"text/javascript\">alert('现在进入作品上传!');document.location.href='UP.jsp?openid="+openid+"';</script>");
